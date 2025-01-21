c4b37f7c2257424fd963770cd4bb511fad3c6d44
nono <nonokub.671@gmail.com>
2025-01-21 21:59:48 +0700

    fix:staff router fix

--- routers/staff.go
+++ routers/staff.go
@@ -6,11 +6,11 @@ import (
 )
 
 func SetupStaffRouter(r *gin.RouterGroup, handler handlers.StaffHandler) {
-	staffRouteV1 := r.Group("/staff")
+	staffRouteV1 := r.Group("/v1/staffs")
 	{
 		staffRouteV1.GET("/:id", handler.GetStaffById)
 		staffRouteV1.GET("/program/:program_id", handler.GetStaffByProgramId)
 		staffRouteV1.POST("/", handler.CreateStaff)
-		staffRouteV1.PUT("", handler.UpdateStaff)
+		staffRouteV1.PUT("/", handler.UpdateStaff)
 	}
 }
